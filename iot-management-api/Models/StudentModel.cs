fa32c547ef7586405beb1245bfd8a5e7b5a41c8f
Maxym Perebiynis <perebyinis.maksym@lll.kpi.ua>
2023-10-02 13:37:40 +0300

    models for responses

---
+++ iot-management-api/Models/StudentModel.cs
@@ -0,0 +1,10 @@
+﻿using iot_management_api.Models.common;
+
+namespace iot_management_api.Models
+{
+
+    public class StudentModel : UserModel
+    {
+        public required string GroupCode { get; set; }
+    }
+}
