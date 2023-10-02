a958124a5acdda267961b10547983f6f32e9379b
Maxym Perebiynis <perebyinis.maksym@lll.kpi.ua>
2023-10-02 13:38:07 +0300

    sign up/in (different endpoints for users)

--- iot-management-api/Controllers/AuthController.cs
+++ iot-management-api/Controllers/AuthController.cs
@@ -11,119 +11,169 @@ namespace iot_management_api.Controllers
     [Route("api/auth")]
     public class AuthController : Controller
     {
-        private readonly IUserService _userService;
+        private readonly ITeacherService _teacherService;
+        private readonly IStudentService _studentService;
         private readonly ILogger<AuthController> _logger;
         private readonly JwtGenerator _jwtGenerator;
         private readonly IMapper _mapper;
 
-        public AuthController(IUserService userService,
+        public AuthController(ITeacherService teacherService,
+            IStudentService studentService,
             ILogger<AuthController> logger,
             JwtGenerator jwtGenerator,
             IMapper mapper)
         {
-            _userService = userService;
+            _teacherService=teacherService;
+            _studentService=studentService;
             _logger=logger;
             _jwtGenerator=jwtGenerator;
             _mapper=mapper;
         }
 
-        public class RegRequest
-        {
-            public required RegUser User { get; set; }
-            public string? GroupCode { get; set; }
-            public class RegUser
-            {
-                public required string Name { get; set; }
-                public required string Surname { get; set; }
-                public required string Email { get; set; }
-                public required string Password { get; set; }
-                public required string Role { get; set; }
-            }
-        }
-        public class RegResponse
-        {
-            public int? UserId { get; set; }
-        }
-
         [HttpPost]
-        [Route("register")]
-        [ProducesResponseType(typeof(RegResponse), 200)]
-        public async Task<IActionResult> Register([FromBody] RegRequest request)
+        [Route("signup/student")]
+        [ProducesResponseType(typeof(SignUpResponse), 200)]
+        public async Task<IActionResult> SignUpStudent([FromBody] StudentSignUpRequest request)
         {
-            var createdId = await _userService.CreateAsync(_mapper.Map<User>(request.User), request.GroupCode);
+            var createdId = await _studentService.CreateAsync(_mapper.Map<Student>(request.User), request.GroupCode);
 
-            if (createdId==null)
+            if (createdId == null)
             {
                 _logger.LogError($"User was not created");
                 return BadRequest("Unknown Error. User was not created");
             }
 
-            return Ok(new RegResponse
+            return Ok(new SignUpResponse
             {
-                UserId = createdId
+                CreatedId = createdId
             });
         }
 
-        /// <summary>
-        ///     Login method, on Output gives Auth Token in Body and in Cookies("auth_token")
-        /// </summary>
-        /// <param name="user">User Entity</param>
-        /// <returns>User Model, Auth Token in Body and in Cookies("auth_token")</returns>
         [HttpPost]
-        [Route("login")]
-        [ProducesResponseType(typeof(LoginResponse), 200)]
-        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        [Route("signup/teacher")]
+        [ProducesResponseType(typeof(SignUpResponse), 200)]
+        public async Task<IActionResult> SignUpTeacher([FromBody] TeacherSignUpRequest request)
         {
-            if (!Enum.TryParse<UserRole>(request.User.Role, out var userRole))
-                return BadRequest("User role not exists");
+            var createdId = await _teacherService.CreateAsync(_mapper.Map<Teacher>(request.User));
 
-            var dbUser = await _userService.GetByEmail(request.User.Email, userRole);
+            if (createdId == null)
+            {
+                _logger.LogError($"User was not created");
+                return BadRequest("Unknown Error. User was not created");
+            }
 
-            if (dbUser == null)
+            return Ok(new SignUpResponse
+            {
+                CreatedId = createdId
+            });
+        }
+
+        [HttpPost]
+        [Route("signin/student")]
+        [ProducesResponseType(typeof(StudentSignInResponse), 200)]
+        public async Task<IActionResult> SignInStudent([FromBody] SignInRequest request)
+        {
+            //user = await _teacherService.GetByEmail(request.User.Email);
+            User? user = await _studentService.GetByEmail(request.User.Email);
+
+            if (user == null)
             {
                 return BadRequest("User not found");
             }
 
-            if (!BCrypt.Net.BCrypt.Verify(request.User.Password, dbUser.Password))
+            //password check
+            if (!BCrypt.Net.BCrypt.Verify(request.User.Password, user.Password))
             {
-                _logger.LogWarning($"User(Id={dbUser.Id}) password is not correct");
+                _logger.LogWarning($"User(Id={user.Id}) password is not correct");
                 return BadRequest("Password not correct.");
             }
-            _logger.LogInformation($"User(Id={dbUser.Id}) password is correct");
+            _logger.LogInformation($"User(Id={user.Id}) password is correct");
 
-            var token = _jwtGenerator.GenerateToken(dbUser.Id);
+            //token gen
+            var token = _jwtGenerator.GenerateToken(user.Id, UserRole.Student);
             HttpContext.Response.Cookies.Append("auth_token", token);
 
-            return Ok(new LoginResponse
+            //response
+            return Ok(new StudentSignInResponse
             {
                 Token = token,
-                User = _mapper.Map<UserModel>(dbUser)
-                //User = new UserModel
-                //{
-                //    Id = dbUser.Id,
-                //    Name = dbUser.Name,
-                //    Surname = dbUser.Surname,
-                //    Email = dbUser.Email,
-                //    Role = dbUser.Role.ToString(),
-                //}
+                User = _mapper.Map<StudentModel>(user)
             });
         }
-        public class LoginRequest
+
+        [HttpPost]
+        [Route("signin/teacher")]
+        [ProducesResponseType(typeof(TeacherSignInResponse), 200)]
+        public async Task<IActionResult> SignInTeacher([FromBody] SignInRequest request)
         {
-            public required LoginUser User { get; set; }
-            public class LoginUser
+            User? user = await _teacherService.GetByEmail(request.User.Email);
+
+            if (user == null)
+            {
+                return BadRequest("User not found");
+            }
+
+            //password check
+            if (!BCrypt.Net.BCrypt.Verify(request.User.Password, user.Password))
+            {
+                _logger.LogWarning($"User(Id={user.Id}) password is not correct");
+                return BadRequest("Password not correct.");
+            }
+            _logger.LogInformation($"User(Id={user.Id}) password is correct");
+
+            //token gen
+            var token = _jwtGenerator.GenerateToken(user.Id, UserRole.Teacher);
+            HttpContext.Response.Cookies.Append("auth_token", token);
+
+            //response
+            return Ok(new TeacherSignInResponse
+            {
+                Token = token,
+                User = _mapper.Map<TeacherModel>(user)
+            });
+        }
+
+        public class UserSignUp
+        {
+            public required string Name { get; set; }
+            public required string Surname { get; set; }
+            public required string Email { get; set; }
+            public required string Password { get; set; }
+        }
+        public class StudentSignUpRequest
+        {
+            public required UserSignUp User { get; set; }
+            public string? GroupCode { get; set; }
+        }
+        public class TeacherSignUpRequest
+        {
+            public required UserSignUp User { get; set; }
+        }
+        public class SignUpResponse
+        {
+            public int? CreatedId { get; set; }
+        }
+
+        public class SignInRequest
+        {
+            public required SignInUser User { get; set; }
+            public class SignInUser
             {
                 public required string Email { get; set; }
                 public required string Password { get; set; }
-                public required string Role { get; set; }
             }
-
         }
 
-        public class LoginResponse
+        public class StudentSignInResponse
         {
             public required string Token { get; set; }
-            public required UserModel User { get; set; }
+            public required StudentModel User { get; set; }
+        }
+
+        public class TeacherSignInResponse
+        {
+            public required string Token { get; set; }
+            public required TeacherModel User { get; set; }
         }
     }
 }
